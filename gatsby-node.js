687c9a32918927fde50b81c9b7885d89f1f1cf0a
Karina Chow <karina.chow@mutations.ltd>
2020-11-03 17:04:19 -0800

    Kill blog page

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,46 +4,6 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// exports.createPages = ({ graphql, actions }) => {
-//   const { createPage } = actions
-
-//   return new Promise((resolve, reject) => {
-//     const blogPost = path.resolve('./src/templates/blog-post.js')
-//     resolve(
-//       graphql(
-//         `
-//           {
-//             allContentfulBlogPost {
-//               edges {
-//                 node {
-//                   title
-//                   slug
-//                 }
-//               }
-//             }
-//           }
-//           `
-//       ).then(result => {
-//         if (result.errors) {
-//           console.log(result.errors)
-//           reject(result.errors)
-//         }
-
-//         const posts = result.data.allContentfulBlogPost.edges
-//         posts.forEach((post, index) => {
-//           createPage({
-//             path: `/blog/${post.node.slug}/`,
-//             component: blogPost,
-//             context: {
-//               slug: post.node.slug
-//             },
-//           })
-//         })
-//       })
-//     )
-//   })
-// }
-
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === 'build-html') {
     actions.setWebpackConfig({
